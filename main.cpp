5a6d3aa72c2fcf305db58e5b5ca249817bcf40d4
Sion Pixley <sionpixley@gmail.com>
2020-03-15 16:22:08 -0500

    Created clear() function.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,17 @@ int main()
     {
         v.push_back("hello");
     }
-    for(int i = 0; i < v.length(); i += 1)
+    for(int i = 0; i < v.size(); i += 1)
     {
         cout << v[i] << endl;
     }
+    
+    std::string array[5] = { "4", "5", "6", "7", "8" };
+    v = array;
+    for(int i = 0; i < v.size(); i += 1)
+    {
+        cout << v[i] << endl;
+    }
+    
     return 0;
 }
