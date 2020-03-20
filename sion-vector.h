79ad9779075cc73ccb4148b355d8ac4bf99aa65c
Sion Pixley <sionpixley@gmail.com>
2020-03-20 14:39:02 -0500

    Experimenting with iterator stuff.

--- sion-vector.h
+++ sion-vector.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <iterator>
+#include <algorithm>
 
 
 using std::cout;
@@ -11,7 +13,7 @@ using std::endl;
 
 namespace sion
 {
-    template<typename type>
+    template<class type>
     class vector
     {
     private:
@@ -20,6 +22,72 @@ namespace sion
         size_t length;
         
     public:
+        template<class T>
+        class vector_iter
+        {
+        private:
+            T* pointee;
+            
+        public:
+            using iterator_category = std::random_access_iterator_tag;
+            using value_type = T;
+            using difference_type = std::ptrdiff_t;
+            using pointer = T*;
+            using reference = T&;
+            
+            vector_iter()
+            {
+                pointee = nullptr;
+            }
+            
+            vector_iter(T* ptr)
+            {
+                pointee = ptr;
+            }
+            
+            T& operator *()
+            {
+                return *pointee;
+            }
+            
+            T* operator ->()
+            {
+                return pointee;
+            }
+            
+            vector_iter<T>& operator ++()
+            {
+                ++pointee;
+                return *this;
+            }
+            
+            vector_iter<T>& operator --()
+            {
+                --pointee;
+                return *this;
+            }
+            
+            vector_iter<T>& operator +(int rhs)
+            {
+                pointee = pointee + rhs;
+                return *this;
+            }
+            
+            vector_iter<T>& operator -(int rhs)
+            {
+                pointee = pointee - rhs;
+                return * this;
+            }
+            
+            bool operator ==(vector_iter<T>& rhs)
+            {
+                return this.pointee == rhs.pointee;
+            }
+        };
+        
+        
+        using iterator = vector_iter<type>;
+        
         vector()
         {
             max_length = 10;
@@ -72,7 +140,7 @@ namespace sion
             else
             {
                 type t = elements[length - 1];
-                elements[length - 1] = NULL;
+                elements[length - 1] = nullptr;
                 length -= 1;
                 return t;
             }
@@ -92,6 +160,16 @@ namespace sion
             {
                 return 1;
             }
+        }
+        
+        iterator begin()
+        {
+            return iterator(&elements[0]);
+        }
+        
+        iterator end()
+        {
+            return iterator(&elements[length - 1] + 1);
         }
         
         type& operator[](size_t index)
